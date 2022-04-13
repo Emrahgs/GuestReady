4aa48fad4c661d962eaf60b4360321f94724f5a6
Emrahgs <bagirovemrah97@gmail.com>
2022-04-14 00:30:11 +0400

    done

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import datetime
 
-commission = 0
-
 def get_commission(date_str):
-    format = '%Y-%m-%d'
-    months = datetime.datetime.strptime(input, format).month
-    if 6<=months<=9:
-        commission = 0.15
-    else:
-        commission = 0.1
+    date = '%Y-%m-%d'
+    commission = 10
+    try:
+        months = datetime.datetime.strptime(input, date).month
+        if 6<=months<=9:
+            commission = 15
+    except ValueError:
+        return ValueError('Please, enter a valid date')    
     return float(commission)
     
-input = ('2022-7-13')
-
+input = ('2022-6-13')
 print(get_commission(input))
